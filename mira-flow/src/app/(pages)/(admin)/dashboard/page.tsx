bedd41d77e8289fc595087ac27bccd8fe991350f
Lukayx <olavarriafernando36@gmail.com>
2024-11-15 22:03:15 -0300

    Database y dashboard distinto

--- mira-flow/src/app/(pages)/(admin)/dashboard/page.tsx
+++ mira-flow/src/app/(pages)/(admin)/dashboard/page.tsx
@@ -3,6 +3,28 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <h1>hola</h1>
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+
+        <p className={styles.description}>
+          Get started by editing <code className={styles.code}>pages/index.js</code>
+        </p>
+
+        <div className={styles.grid}>
+          <a href="https://nextjs.org/docs" className={styles.card}>
+            <h2>Documentation &rarr;</h2>
+            <p>Find in-depth information about Next.js features and API.</p>
+          </a>
+
+          <a href="https://nextjs.org/learn" className={styles.card}>
+            <h2>Learn &rarr;</h2>
+            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          </a>
+        </div> {/* Cierre correcto del div */}
+      </main>
+    </div>
   );
 }
