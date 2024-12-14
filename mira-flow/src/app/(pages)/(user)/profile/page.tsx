4008348af1a23bf293f16721a1d63eb974fce31c
lucianouu <luciano.espinoza@alumnos.uach.cl>
2024-12-14 20:48:24 -0300

    profile pc

--- mira-flow/src/app/(pages)/(user)/profile/page.tsx
+++ mira-flow/src/app/(pages)/(user)/profile/page.tsx
@@ -3,6 +3,28 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <h1>PERFIL</h1>
+    <div className={styles.container}>
+      <div className={styles.profileCard}>
+        <div className={styles.avatar}></div>
+        <div className={styles.profileInfo}>
+          <h2 className={styles.name}>Juan</h2>
+          <p className={styles.email}>
+            <span className={styles.icon}>✉</span> correo@gmail.com
+          </p>
+          <p className={styles.phone}>
+            <span className={styles.icon}>📞</span> +569 XXXXXXXXX
+          </p>
+          <p className={styles.description}>
+            Descripción de Juan, ayuda nose que mas poner aquí
+          </p>
+        </div>
+        <div className={styles.editIcon}>✏️</div>
+      </div>
+      <div className={styles.buttonContainer}>
+        <button className={styles.publishButton}>
+          <span>➕</span> Publicar Anuncio
+        </button>
+      </div>
+    </div>
   );
 }
