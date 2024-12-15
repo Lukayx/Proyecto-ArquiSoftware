af95ed21681a568c075fd90da6c878eabe6a30ae
Lukayx <olavarriafernando36@gmail.com>
2024-12-14 21:45:27 -0300

    CAMBIOS USER, INTERFAZ HOME FIGMA

--- mira-flow/src/app/(pages)/(user)/nav.tsx
+++ mira-flow/src/app/(pages)/(user)/nav.tsx
@@ -1,15 +1,32 @@
 import Link from "next/link";
 import styles from "./page.module.css"; // Asegúrate de que el nombre y la ruta del archivo sean correctos
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faShoppingCart} from "@fortawesome/free-solid-svg-icons";
 
 const Nav = () => {
   return (
     <nav className={styles.nav}>
-      <ul className={styles.navList}>
-        <li><Link href="/main" className={styles.navLink}>Home</Link></li>
-        <li><Link href="/products" className={styles.navLink}>Mis productos</Link></li>
-        <li><Link href="/sales" className={styles.navLink}>Ventas</Link></li>
-        <li><Link href="/profile" className={styles.navLink}>Perfil</Link></li>
-      </ul>
+      <ul className={styles.seccionesHeader}>
+          <div className={styles.seccionesHeaderIzquierda}>
+            <li>
+              <h1>Miraflow</h1>
+            </li>
+          </div>
+          <div className={styles.seccionesHeaderDerecha}>
+            <li>
+              <a href="/admin/dashboard">
+                Mis Productos 
+                <FontAwesomeIcon icon={faShoppingCart} className={styles.icon}/>
+              </a>
+            </li>
+            <li>
+              <a href="/admin/users">
+                Perfil
+                <FontAwesomeIcon icon={faUser} className={styles.icon}/>
+              </a>
+            </li>
+          </div>
+        </ul>
     </nav>
   );
 };
