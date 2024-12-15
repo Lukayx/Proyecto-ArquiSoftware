307da03a85e7d08fcdbf42b8ab1850d77f0e5da5
lucianouu <luciano.espinoza@alumnos.uach.cl>
2024-12-15 00:10:49 -0300

    profile ahora sikesi

--- mira-flow/src/app/(pages)/(user)/profile/page.tsx
+++ mira-flow/src/app/(pages)/(user)/profile/page.tsx
@@ -1,28 +1,48 @@
-import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faPhone, faPencilAlt, faPlus } from "@fortawesome/free-solid-svg-icons";
 import styles from "./page.module.css";
+import photo from "../main/assets/juan.jpg";
+
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <div className={styles.profileCard}>
-        <div className={styles.avatar}></div>
+        <div className={styles.avatar}>
+        <img 
+            src={photo.src}
+            alt="Avatar de Juan" 
+            className={styles.avatarImage} 
+          />
+        </div>
         <div className={styles.profileInfo}>
           <h2 className={styles.name}>Juan</h2>
           <p className={styles.email}>
-            <span className={styles.icon}>✉</span> correo@gmail.com
+            <span className={styles.icon}>
+              <FontAwesomeIcon icon={faEnvelope} />
+            </span>{" "}
+            correo@gmail.com
           </p>
           <p className={styles.phone}>
-            <span className={styles.icon}>📞</span> +569 XXXXXXXXX
+            <span className={styles.icon}>
+              <FontAwesomeIcon icon={faPhone} />
+            </span>{" "}
+            +569 XXXXXXXXX
           </p>
           <p className={styles.description}>
             Descripción de Juan, ayuda nose que mas poner aquí
           </p>
         </div>
-        <div className={styles.editIcon}>✏️</div>
+        <div className={styles.editIcon}>
+          <FontAwesomeIcon icon={faPencilAlt} />
+        </div>
       </div>
       <div className={styles.buttonContainer}>
         <button className={styles.publishButton}>
-          <span>➕</span> Publicar Anuncio
+          <span className={styles.icon}>
+            <FontAwesomeIcon icon={faPlus} />
+          </span>{" "}
+          Publicar Anuncio
         </button>
       </div>
     </div>
