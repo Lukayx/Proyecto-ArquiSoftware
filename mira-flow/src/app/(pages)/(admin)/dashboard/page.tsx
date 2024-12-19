5fe91d15d121bdbabbfb6d0d9d72ae8dfed03bca
lucianouu <luciano.espinoza@alumnos.uach.cl>
2024-12-19 19:56:30 -0300

    dashboard admin creado

--- mira-flow/src/app/(pages)/(admin)/dashboard/page.tsx
+++ mira-flow/src/app/(pages)/(admin)/dashboard/page.tsx
@@ -1,35 +1,29 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUsers, faChartLine } from "@fortawesome/free-solid-svg-icons";
+import Nav from '../nav';
 
-const sleep = (ms: number): Promise<void> => new Promise((resolve) => setTimeout(resolve, ms));
-
-export default async function Home() {
-    // await sleep(2000); // Espera 2 segundos
-    // throw new Error("error Eres demasiado wap@");
-
+export default function Administrador() {
   return (
-    <section className={styles.container}>
-      <section className={styles.section1}>
-        {/* Aquí puedes agregar las cartas en filas y columnas */}
-        <div className={styles.card}>Carta 1</div>
-        <div className={styles.card}>Carta 2</div>
-        <div className={styles.card}>Carta 3</div>
-        <div className={styles.card}>Carta 4</div>
-        <div className={styles.card}>Carta 5</div>
-        <div className={styles.card}>Carta 6</div>
-        {/* Agrega más cartas según sea necesario */}
-      </section>
-
-      <section className={styles.section2}>
-        {/* Barra de búsqueda */}
-        <input type="text" placeholder="Buscar..." className={styles.searchBar} />
-        {/* Lista de filtros */}
-        <ul className={styles.filterList}>
-          <li className={styles.filterItem}>Filtro 1</li>
-          <li className={styles.filterItem}>Filtro 2</li>
-          <li className={styles.filterItem}>Filtro 3</li>
-        </ul>
-      </section>
-    </section>
+    <div>
+    <Nav />
+    <div className={styles.container}>
+      <h1 className={styles.title}>ADMINISTRADOR</h1>
+      <div className={styles.buttonContainer}>
+        <a href="/menu">
+          <div className={styles.card}>
+            <FontAwesomeIcon icon={faUsers} className={styles.icon} />
+            <p className={styles.text}>Usuarios</p>
+          </div>
+        </a>
+        <a href="/profits">
+          <div className={styles.card}>
+            <FontAwesomeIcon icon={faChartLine} className={styles.icon} />
+            <p className={styles.text}>Ganancias</p>
+          </div>
+        </a>
+      </div>
+    </div>
+    </div>
   );
 }
